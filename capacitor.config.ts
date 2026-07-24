1505b27d201ac0ba43d1dd3933180d23293b722e
badlaniaarav-tech <badlaniaarav@gmail.com>
2026-07-24 12:21:25 -0400

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.rangecaddy.app',
+  appName: 'Range Caddy',
+  webDir: 'www'
+};
+
+export default config;
